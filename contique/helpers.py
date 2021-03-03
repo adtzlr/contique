84777017cd941d15d55ba7e1222e1593cfe9fcc8
Andreas D <a.dutzler@gmail.com>
2021-03-03 22:29:05 +0100

    Update helpers.py
    documente helper functions

--- contique/helpers.py
+++ contique/helpers.py
@@ -3,26 +3,92 @@
 Created on Wed Feb 17 14:31:04 2021
 
 @author: Andreas
+
+Contique - Numeric Continuation of equilibrium equations
+Copyright (C) 2021 Andreas Dutzler
+
+This program is free software: you can redistribute it and/or modify
+it under the terms of the GNU General Public License as published by
+the Free Software Foundation, either version 3 of the License, or
+(at your option) any later version.
+
+This program is distributed in the hope that it will be useful,
+but WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+GNU General Public License for more details.
+
+You should have received a copy of the GNU General Public License
+along with this program.  If not, see <https://www.gnu.org/licenses/>.
 """
 
 import numpy as np
 
 
 def needle(j, m):
-    "Return zero-vector with needle at component `j` and length `m`."
-    n = np.zeros(m)
+    """Return an array of zeros with a needle at component `j` and length `m`.
+    This array is used to slice out the j-th component of another 1d-array in
+    `xj = needle(j,m).dot(x)`. Furthermore, the derivative of this equation
+    w.r.t. `x` results in `needle(j,m)`.
+
+    Parameters
+    ----------
+    j : int
+        Signed 1-indexed (!) component at which the needle should be positioned.
+    m : int
+        Length of the output array.
+
+    Returns
+    -------
+    ndarray
+        1d-array of length with a needle at item j and zero for all other items.
+
+    Examples
+    --------
+    A needle vector with length m=9 and a needle at j=5.
+
+    >>> needle(-6,9)
+    array([0, 0, 0, 0, 0, 1, 0, 0, 0])
+
+    """
+
+    # init a 1d-array filled with zeros
+    n = np.zeros(m, dtype=int)
+
+    # insert needle at position `j`
     n[abs(j) - 1] = 1
+
     return n
 
 
 def control(x):
-    "Signed index of greatest absolute component of vector x (1-indexed!)."
+    """Obtain the signed index of the greatest absolute value of a
+    1d-array. The returned integer is 1-indexed (!) and multiplied
+    by the sign of the greatest value.
+
+    Parameters
+    ----------
+    x : ndarray
+        1d-array
+
+    Returns
+    -------
+    int
+        1-indexed (!) position of which contains the greatest absolute value
+        of `x` multiplied by the sign of this greatest value.
+    """
+
+    # 0-indexed position
     j = abs(x).argmax()
+
+    # convert to 1-indexed position and multiply by the sign of `x[j]`
     return int((j + 1) * np.sign(x[j]))
 
 
 def argparser(fun):
+    "Function decorator for the handling of function arguments."
+
     def inner(x, *args, **kwargs):
+        "Pass \*args and \*\*kwargs to a function if they are not None."
         if args[0] is None and kwargs is None:
             f = fun(x)
         if args[0] is not None and kwargs is None:
