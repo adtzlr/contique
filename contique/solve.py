90d014b382d699441bac0c7f2b7fc85e7564438e
Andreas D <a.dutzler@gmail.com>
2021-03-05 09:59:33 +0100

    Add docstring to solve.py

--- contique/solve.py
+++ contique/solve.py
@@ -36,8 +36,8 @@ def solve(
     dxmax=0.05,
     dlpfmax=0.05,
     control0="lpf",
-    jacmode=2,
-    jaceps=1e-6,
+    jacmode=3,
+    jaceps=None,
     maxsteps=80,
     maxcycles=4,
     maxiter=20,
@@ -50,7 +50,61 @@ def solve(
     low=1e-6,
     minlastfailed=3,
 ):
-    "Numeric continuation algorithm."
+    """Numeric continuation of (nonlinear) equilibrium equations.
+
+    Parameters
+    ----------
+    fun : function
+        function in terms of unknows x and optional args which returns the
+        equilibrium equations.
+    x0 : ndarray
+        1d-array with initial values of unknows x
+    lpf0 : float
+        initial value for the load-proportionality-factor
+    jac : function, optional
+        jacobian of fun w.r.t. the unknows x
+    args : tuple, optional
+        Optional tuple of arguments which are passed to the function. Eeven if only
+        one argument is passed, it has to be encapsulated in a tuple (default is (None,)).
+    dxmax : float, optional
+        max. allowed absolute incremental increase of unknowns per step
+    dlpfmax : float, optional
+        max. allowed absolute incremental increase of lpf per step
+    control0 : int, optional
+        initial control component ( 1-indexed
+    jacmode : int, optional
+        forward (2) or central (3) finite-differences approx. of the jacobian
+    jaceps : float, optional
+        user-specified stepwidth (if None, this defaults to eps^(1/mode))
+    maxsteps : int, optional
+        max. number of steps
+    maxcycles : int, optional
+        max. number of cycles per step
+    maxiter : int, optional
+        max. number of Newton-iterations per cycle
+    tol : float, optional
+        tolerated residual of the norm of the equilibrium equation (default is 1e-8)
+    overshoot : float, optional
+        allowed overshoot of the final control component of a cycle (default is 1.0)
+    rebalance : bool, optional
+        rebalance max. allowed incremental increase values after each step
+    increase : float, optional
+        rebalance increase factor
+    decrease : float, optional
+        rebalance decrease factor
+    high : float, optional
+        rebalance max. factor of incremental increase w.r.t. to the initial values
+    low : float, optional
+        rebalance min. factor of incremental increase w.r.t. to the initial values
+    minlastfailed : int, optional
+        rebalance increase only after a given number of converged steps
+
+    Returns
+    -------
+    Res : list
+        List of NewtonResults (with res.x being the final unknowns per step)
+
+    """
 
     # init number of unknows
     ncomp = 1 + len(x0)
