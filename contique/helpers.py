8493be0583b44806eca1465beda7b8a84ea4262e
Andreas Dutzler <a.dutzler@gmail.com>
2022-09-05 00:43:27 +0200

    switch to 0-indexed control component
    and introduce a seperate sign variable

--- contique/helpers.py
+++ contique/helpers.py
@@ -5,7 +5,7 @@ Created on Wed Feb 17 14:31:04 2021
 @author: adtzlr
 
 Contique - Numeric continuation of equilibrium equations
-Copyright (C) 2021 Andreas Dutzler
+Copyright (C) 2022 Andreas Dutzler
 
 This program is free software: you can redistribute it and/or modify
 it under the terms of the GNU General Public License as published by
@@ -24,46 +24,54 @@ along with this program.  If not, see <https://www.gnu.org/licenses/>.
 import numpy as np
 
 
-def needle(j, m):
-    """Return an array of zeros with a needle at component `j` and length `m`.
-    This array is used to slice out the j-th component of another 1d-array in
-    `xj = needle(j,m).dot(x)`. Furthermore, the derivative of this equation
-    w.r.t. `x` results in `needle(j,m)`.
+def needle(component: int, length: int):
+    """Return an array of zeros with a needle at a given ``component`` and 
+    ``length``. This array is used to slice out the j-th component of another 
+    1d-array.
+    
+    ..  code-block::
+        
+        x_j = needle(component, length).dot(x)
+    
+    
+    Furthermore, the derivative of this  equation w.r.t. ``x`` results in
+    ``needle(component, length)``.
 
     Parameters
     ----------
-    j : int
-        Signed 1-indexed (!) component at which the needle should be positioned.
-    m : int
+    component : int
+        Component at which the needle should be positioned.
+    length : int
         Length of the output array.
 
     Returns
     -------
     ndarray
-        1d-array of length with a needle at item j and zero for all other items.
+        1d-array with a needle at item ``component`` and zero for all other 
+        items.
 
     Examples
     --------
-    A needle vector with length m=9 and a needle at j=5.
+    A needle vector with ``length = 9`` and a needle at ``component=5``.
 
-    >>> needle(-6,9)
+    >>> needle(5, 9)
     array([0, 0, 0, 0, 0, 1, 0, 0, 0])
 
     """
 
     # init a 1d-array filled with zeros
-    n = np.zeros(m, dtype=int)
+    n = np.zeros(length, dtype=int)
 
-    # insert needle at position `j`
-    n[abs(j) - 1] = 1
+    # insert needle
+    n[component] = 1
 
     return n
 
 
 def control(x):
-    """Obtain the signed index of the greatest absolute value of a
-    1d-array. The returned integer is 1-indexed (!) and multiplied
-    by the sign of the greatest value.
+    """Obtain the index and the sign of the greatest absolute value of a
+    1d-array. The returned integer and the sign are taken from
+    the greatest value.
 
     Parameters
     ----------
@@ -73,15 +81,15 @@ def control(x):
     Returns
     -------
     int
-        1-indexed (!) position of which contains the greatest absolute value
-        of `x` multiplied by the sign of this greatest value.
+        Position which contains the greatest absolute value of `x`.
+    int
+        Sign of the greatest value.
     """
 
     # 0-indexed position
-    j = abs(x).argmax()
+    component = abs(x).argmax()
 
-    # convert to 1-indexed position and multiply by the sign of `x[j]`
-    return int((j + 1) * np.sign(x[j]))
+    return component, int(np.sign(x[component]))
 
 
 def argparser(fun):
